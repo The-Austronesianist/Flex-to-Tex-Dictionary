387fa5176ae98b0983aeb15ccc06f6b329bc9866
Alex Smith <smith.alexander.david@gmail.com>
2022-06-26 11:31:05 +0800

    Update FTT_dict.py
    Put in more loops to grab more things from the html

--- FTT_dict.py
+++ FTT_dict.py
@@ -1,24 +1,13 @@
-#Still in planning stage. Just printing things out to make sure I have the access paths correct. 
+#Still in planning stage. Just printing things out to make sure I have the access paths correct.
+#Got it so that I get everything I want. Now just need to make it write to file.
 
-import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
-import ssl
 import re
-
-#Ignor SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
 
 dictionary_file = open('configured dictionary.xhtml', 'r', encoding='utf-8')
 soup = BeautifulSoup(dictionary_file, 'html.parser')
 
 entry = soup('div')
-
-#TO DO
-#example sentences (look at the entry for von 'bundle')
-#morpheme type (look at the entry for N-)
-
 
 for div in entry :
     if div.get('class') == ['entry'] :
@@ -31,8 +20,17 @@ for div in entry :
                 def_and_pos = tag.children
                 for tag in def_and_pos :
                     if tag.get('class') == ['sharedgrammaticalinfo'] :
-                        wordclass = tag.text
-                        print(wordclass)
+                        wordclass = tag.children
+                        for tag in wordclass :
+                            if tag.get('class') == ['morphosyntaxanalysis'] :
+                                pos_and_morphtype = tag.children
+                                for tag in pos_and_morphtype :
+                                    if tag.get('class') == ['partofspeech'] :
+                                        pos = tag.text
+                                        print(pos)
+                                    if tag.get('class') == ['morphtypes'] :
+                                        morphtype = tag.text
+                                        print(morphtype)
                     if tag.get('class') == ['sensecontent'] :
                         sense_and_def = tag.children
                         for tag in sense_and_def :
@@ -40,5 +38,23 @@ for div in entry :
                                 sense_number = tag.text
                                 print(sense_number)
                             if tag.get('class') == ['sense'] :
-                                definition = tag.text
-                                print(definition)
+                                def_and_pos_sense = tag.children
+                                for tag in def_and_pos_sense :
+                                    if tag.get('class') == ['partofspeech'] :
+                                        sense_pos = tag.text
+                                        print(tag)
+                                    if tag.get('class') == ['definitionorgloss'] :
+                                        definition = tag.text
+                                        print(definition)
+                                    if tag.get('class') == ['examplescontents'] :
+                                        examples = tag.children
+                                        for tag in examples :
+                                            if tag.get('class') == ['examplescontent'] :
+                                                example = tag.children
+                                                for tag in example :
+                                                    if tag.get('class') == ['example'] :
+                                                        entry_ex = tag.text
+                                                        print(entry_ex)
+                                                    if tag.get('class') == ['translationcontents'] :
+                                                        entry_ex_trans = tag.text
+                                                        print(entry_ex_trans)
